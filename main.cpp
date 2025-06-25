01d0654f2cbfc718338984284cb675ceb60e1395
darkluke210@hotmail.co.uk <darkluke210@hotmail.co.uk>
2025-06-25 15:07:57 +0100

    added power and relu functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <stack>
 #include <vector>
+#include <math.h>
 
 class Value : public std::enable_shared_from_this<Value>{
     public: 
@@ -14,23 +15,43 @@ class Value : public std::enable_shared_from_this<Value>{
         Value(float x) : data(x), grad(0), op("") {}
 
         // Arithmetic Operators
+        // Addition
         std::shared_ptr<Value> operator+(std::shared_ptr<Value> const& other){
             auto res = std::make_shared<Value>(other->data + this->data);
             res->op = "+";
             res->left = shared_from_this();
             res->right = other;
-            // std::cout << "Value(data= " << this->data << " | grad= " << this->grad << ") " << res->op << " Value(data= " << other->data << " | grad= " << other->grad << ")" << std::endl; 
+            std::cout << "Value(data= " << this->data << " | grad= " << this->grad << ") " << res->op << " Value(data= " << other->data << " | grad= " << other->grad << ")" << std::endl; 
             return res;
         }
-
+        // Multiply
         std::shared_ptr<Value> operator*(std::shared_ptr<Value> const& other){
             auto res = std::make_shared<Value>(other->data * this->data);
             res->op = "*";
             res->left = shared_from_this();
             res->right = other;
-            // std::cout << "Value(data= " << this->data << " | grad= " << this->grad << ") "<< res->op << " Value(data= " << other->data << " | grad= " << other->grad << ")" << std::endl; 
+            std::cout << "Value(data= " << this->data << " | grad= " << this->grad << ") "<< res->op << " Value(data= " << other->data << " | grad= " << other->grad << ")" << std::endl; 
             return res;
         }
+        // Exponent
+        std::shared_ptr<Value> power(const std::shared_ptr<Value>& base, float power){
+            auto res = std::make_shared<Value>( std::pow(base->data, power) );
+            res->op = "^";
+            res->left = base;
+            res->right = std::make_shared<Value>(power);
+            std::cout << "Value(data= " << base->data << " | grad= " << base->grad << ") "<< res->op << " float(data= " << power << ")" << std::endl; 
+            return res;
+        }
+        // ReLU
+        std::shared_ptr<Value> relu(const std::shared_ptr<Value>& in){
+            auto out = std::make_shared<Value>(std::max(0.f, in->data));
+            out->op = "relu";
+            out->left = in;
+            out->right = nullptr;
+            std::cout << "Value(data= " << out->op << "( " << in->data << " )= " << out->data << " | grad= " << out->grad << ") "<< std::endl; 
+            return out;
+        }
+
 
         // ostream
         friend std::ostream& operator<<(std::ostream& out, const std::shared_ptr<Value>& obj){
@@ -70,13 +91,22 @@ std::vector<std::shared_ptr<Value>> backward(std::shared_ptr<Value> head){
             if ( v->left ) v->left->grad += v->right->data * v->grad;
             if ( v->right ) v->right->grad += v->left->data * v->grad;
         }
+        else if( v->op == "^"){
+            if ( v->left ) v->right->grad = 0;
+            if ( v->right ) v->left->grad += v->right->data * std::pow(v->left->data, v->right->data - 1) *v->grad;
+        }
+        else if( v->op == "relu"){
+            if( v->left ) v->left->grad = 1 * v->grad;
+        }
         std::cout << "Value(data= " << v->data << " | grad= " << v->grad << ") "<< std::endl;    
     }
     return topo;
 
 }
 
-// Wrappers 
+// Wrappers
+
+
 // Addition
 std::shared_ptr<Value> operator+(
     const std::shared_ptr<Value>& lhs,
@@ -107,6 +137,13 @@ std::shared_ptr<Value> operator*(float lhs, const std::shared_ptr<Value>& rhs) {
     return std::make_shared<Value>(lhs)->operator*(rhs);
 }
 
+std::shared_ptr<Value> power(const std::shared_ptr<Value>& base, float power) {
+    return base->power(base, power);
+}
+
+std::shared_ptr<Value> relu(const std::shared_ptr<Value>& in) {
+    return in->relu(in);
+}
 
 int main() {
     // Eigen::VectorXd x(2);
@@ -130,7 +167,8 @@ int main() {
     auto e = a * b;      // e = a * b
     auto d = e + c;         // d = e + c
     auto L = d * f;         // L = d * f
-    auto T = L * 1.0f;
+    auto T = power(L, 1.0f);
+    auto U = relu(T);
     // L = ((a2*b-3)+c10) * f-2
     //              -8 L
     //           -2 f  *  4 d
@@ -142,7 +180,7 @@ int main() {
     // dL/db = dL/de * de/db = (f ) * a
     // dL/da = dL/de * de/da = (f ) * b
 
-    std::vector<std::shared_ptr<Value>> order = backward(T);
+    std::vector<std::shared_ptr<Value>> order = backward(U);
     std::cout << std::endl;
     // for(int i = 0; i < order.size(); i++){
     //     std::cout << "Value(data= " << order[i]->data << " | grad= " << order[i]->grad << ") Leaf nodes: "<< order[i]->left->data <<", " << order[i]->right->data <<  std::endl; 
